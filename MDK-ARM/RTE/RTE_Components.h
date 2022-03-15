9c705aac98e7194fcc74dc6b612f0914b90a26b4
baihuziyou <1031512314@qq.com>
2022-03-15 20:55:07 +0800

    remotecontrol

---
+++ MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'RemoteControlC8T6' 
+ * Target:  'RemoteControlC8T6' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
